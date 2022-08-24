fd34fb3c34ae2dbdddda995274ec74aafc18c17a
Mark Nelson <mbnelson@pdxt.com>
2022-08-24 09:46:35 -0700

    Project creation

---
+++ CascadiaCrossing/CascadiaCrossingApp.swift
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 ___ORGANIZATIONNAME___. All rights reserved.
+
+import SwiftUI
+
+@main
+struct CascadiaCrossingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CascadiaCrossing/ContentView.swift
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 ___ORGANIZATIONNAME___. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
