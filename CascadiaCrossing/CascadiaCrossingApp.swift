7b325aec7f3ad6379c736683a2cdb25a30e473f8
markpdxt <mbnelson@pdxt.com>
2024-08-20 15:20:42 -0700

    Remove obolete ferry feature

--- CascadiaCrossing/CascadiaCrossingApp.swift
+++ CascadiaCrossing/CascadiaCrossingApp.swift
@@ -1,18 +1,12 @@
-// Copyright (c) 2022 PDX Technologies, LLC. All rights reserved.
+// Copyright (c) 2024 PDX Technologies, LLC. All rights reserved.
 
 import SwiftUI
 
 @main
 struct CascadiaCrossingApp: App {
-  @AppStorage(StorageName.mode.rawValue) private var mode: String?
-
   var body: some Scene {
     WindowGroup {
-      if mode == nil {
-        BorderView()
-      } else {
-        FerryView()
-      }
+      BorderView()
     }
   }
 }
--- CascadiaCrossing/Helpers/StorageName.swift
+++
@@ -1,6 +0,0 @@
-// Created 2022 by DroneLytics.
-
-enum StorageName: String {
-  case mode = "FERRY_MODE"
-  case selection = "FERRY_TERMINAL_SELECTION"
-}
